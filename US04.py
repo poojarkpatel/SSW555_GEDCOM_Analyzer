829c3271cb1f7d7e8e9358b90aba4a2797d18d6d
Varun-Mullins <57276726+Varun-Mullins@users.noreply.github.com>
2020-10-05 17:46:30 -0400

    User Stories us01 and us04 for Sprint 1

---
+++ US04.py
@@ -0,0 +1,10 @@
+"""File with the User Story US04"""
+
+
+def us04(family):
+    """Function to implement user story US04"""
+    for item in family.values():
+        if item._divorced != 'NA':
+            if item._married > item._divorced:
+                """Compares the marriage and the divorce dates"""
+                yield f"US04: This family id {item._family} has an illegal dates for marriage and divorce"
