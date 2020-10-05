427d3b28ecd5a3e982ae2018d8dc90da2aea66fc
sprabhu5 <58062934+sprabhu5@users.noreply.github.com>
2020-10-05 17:45:34 -0400

    Create US_07_test.py

---
+++ US_07_test.py
@@ -0,0 +1,18 @@
+import unittest
+from US_07 import US_07
+from Base_File import Repository
+
+
+class Test(unittest.TestCase):
+
+    def test_US_07(self):
+        """ Contains test cases for US_07"""
+        indi_repo: Repository = Repository("US_07.ged")
+
+        expected = ["Tia /Ale/"]
+
+        self.assertEqual(US_07(indi_repo._individual), expected )
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
