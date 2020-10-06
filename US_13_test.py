3ddd76fe0c977e6ae7be808888a31002005c1a54
3lton007 <37709148+3lton007@users.noreply.github.com>
2020-10-06 00:01:35 -0400

    Add files via upload
    Added US_13 with test cases!

---
+++ US_13_test.py
@@ -0,0 +1,19 @@
+import unittest
+from US_13 import US_13
+from Base_File import Repository
+from typing import List
+
+
+class Test(unittest.TestCase):
+    """Helps to test all the functions"""
+
+    def test_US_13(self):
+        """ The function helps to test US_13 function"""
+        indi_repo: Repository = Repository("ssw555_input_file.ged")
+        expected = {'The family id @F8@ has twins Emmy /Robinson/ and Jil /Robinson/', 'The family id @F8@ has twins Jil /Robinson/ and Sam /Robinson/', 'The family id @F8@ has twins Emmy /Robinson/ and Sam /Robinson/'}
+        self.assertEqual(US_13(indi_repo._family,indi_repo._individual), expected)
+    
+        
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
