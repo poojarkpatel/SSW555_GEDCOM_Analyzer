1fc26bf4930d71dbcc55f412a9339f8db15b72a3
HeliPatel98 <63365064+HeliPatel98@users.noreply.github.com>
2020-10-26 21:25:47 -0400

    Delete US_29.py

--- UserStories/US_29.py
+++
@@ -1,6 +0,0 @@
-def US_29(self):
-    result=[]
-    for key, individual in self._individual.items():
-        if individual._death_date != 'NA':
-            result.append(individual._name)
-    return(result)
