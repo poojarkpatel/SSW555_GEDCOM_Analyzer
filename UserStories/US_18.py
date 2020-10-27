901569a1a5f9ea2ac0ffc82564601b823020ed90
3lton007 <37709148+3lton007@users.noreply.github.com>
2020-10-26 21:23:55 -0400

    Update US_18.py

--- UserStories/US_18.py
+++ UserStories/US_18.py
@@ -12,9 +12,11 @@ def US_18(family_object, individual):
                     for attribute in family_object.values():
                         husband_name = attribute._husband_name
                         wife_name = attribute._wife_name
-                        
+
                         if (individual[person_one_id]._name == husband_name and individual[person_two_id]._name == wife_name) or (individual[person_one_id]._name == wife_name and individual[person_two_id]._name == husband_name):
-                            warnings.append(f"{person_one_id} and {person_two_id} are siblings and a couple. Line number: {attribute.get_line_numbers()['wife_id']}")
+                            array = [person_one_id, person_two_id]
+                            sorted(array)
+                            warnings.append(f"{array[0]} and {array[1]} are siblings and a couple. Line number: {attribute.get_line_numbers()['wife_id']}")
 
     return warnings
 
